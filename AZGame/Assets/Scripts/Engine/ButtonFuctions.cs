7f569078bf4b6c0391cffd49e31cbdb08ab667c7
DSiemienik <dsiemienik@gmail.com>
2017-08-04 21:08:24 +0200

    Showing mission board animation

--- AZGame/Assets/Scripts/Engine/ButtonFuctions.cs
+++ AZGame/Assets/Scripts/Engine/ButtonFuctions.cs
@@ -8,6 +8,8 @@ public class ButtonFuctions : MonoBehaviour {
     [SerializeField] Animator cameraAnimator;
     [SerializeField] GameObject cameraParent;
 
+    private bool isActualMissionShowed;
+
     public void GoToTavern()
     {
         cameraParent.transform.position = new Vector3(180.9f, -25.98f, -18.8f);
@@ -19,5 +21,20 @@ public class ButtonFuctions : MonoBehaviour {
         cameraParent.transform.position = new Vector3(0f, 0f, 0f);
         cameraParent.transform.Rotate(new Vector3(0f, -25.3f, 0f));
         cameraAnimator.Play("GoToMainMenu");
+    }
+    public void SeeActualMissions()
+    {
+        if(!isActualMissionShowed)
+        {
+            isActualMissionShowed = true;
+            cameraParent.transform.position = new Vector3(183.6f, -27.7f, -10.5f);
+            cameraAnimator.Play("ShowMissions");
+        }
+        else
+        {
+            isActualMissionShowed = false;
+            cameraParent.transform.position = new Vector3(180.9f, -25.98f, -18.8f);
+            cameraAnimator.Play("BackFromMissionsBoard");
+        }
     }
 }
