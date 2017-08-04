9edfe5a2bd751310457872ab6633d3a595b0b5ac
DSiemienik <dsiemienik@gmail.com>
2017-08-04 20:05:27 +0200

    Go to tavern animation, add tavern playground, add menu buttons

---
+++ AZGame/Assets/Scripts/Engine/ButtonFuctions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonFuctions : MonoBehaviour {
+
+    [SerializeField] Animator cameraAnimator;
+    [SerializeField] GameObject cameraParent;
+
+    public void GoToTavern()
+    {
+        //cameraAnimator.GetComponent<Transform>().position = new Vector3(180.9f, -25.98f, -18.8f);
+        cameraParent.transform.position = new Vector3(180.9f, -25.98f, -18.8f);
+        cameraParent.transform.Rotate(new Vector3(0f, 25.3f, 0f));
+        //cameraParent.transform.localRotation
+        //cameraAnimator.GetComponent<Transform>().rotation = new Quaternion(0f, 25.3f, 0f,0f);
+        cameraAnimator.Play("GoToTavern");
+    }
+}
